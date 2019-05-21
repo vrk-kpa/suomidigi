76151c7528089f6dc0f216133015cbfa88ab49b1
Arto Iijalainen <arto.iijalainen@gmail.com>
2019-05-21 11:09:56 +0300

    Spaces -> tabs.

--- tools/make/project/robots.mk
+++ tools/make/project/robots.mk
@@ -3,6 +3,6 @@ BUILD_TARGETS += copy-robots
 PHONY += copy-robots
 copy-robots: ## Copy robots.txt
 ifeq ($(ENV),testing)
-        $(call colorecho, "\n-Copying robots.txt for testing env.\n")
-        cp ./conf/env_testing/robots.txt ./public/robots.txt
+	$(call colorecho, "\n-Copying robots.txt for testing env.\n")
+	cp ./conf/env_testing/robots.txt ./public/robots.txt
 endif
