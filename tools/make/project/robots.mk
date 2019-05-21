ba3f2f63b9ad683c1ad4d2f772e825c2960264c2
Arto Iijalainen <arto.iijalainen@gmail.com>
2019-05-21 11:00:17 +0300

    Modified robots.txt copying for testing environment.

--- tools/make/project/robots.mk
+++ tools/make/project/robots.mk
@@ -1,10 +1,8 @@
-ifeq (${ENV}, dev)
-	BUILD_TARGETS += copy-robots
-	PHONY += copy-robots
-	ROBOTS_SOURCE := ./conf/env_development/robots.txt
-	ROBOTS_EXIST := $(shell test -f $(ROBOTS_SOURCE) && echo yes || echo no)
-endif
+BUILD_TARGETS += copy-robots
 
+PHONY += copy-robots
 copy-robots: ## Copy robots.txt
-	$(call colorecho, "\n-Copying robots.txt from ${ROBOTS_SOURCE} to ./public/robots.txt .\n")
-	cp ${ROBOTS_SOURCE} ./public/robots.txt
+ifeq ($(ENV),testing)
+        $(call colorecho, "\n-Copying robots.txt for testing env.\n")
+        cp ./conf/env_testing/robots.txt ./public/robots.txt
+endif
