c464d6718a5944ddf1e8bbfd61a084e239f7e2d9
Sebastian <sebastian@hamse.fi>
2019-07-05 14:30:20 +0300

    Add reading time calculator

---
+++ public/modules/custom/suopa_editorial/src/Plugin/CKEditorPlugin/ReadTime.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\suopa_editorial\Plugin\CKEditorPlugin;
+
+use Drupal\ckwordcount\Plugin\CKEditorPlugin\Wordcount;
+
+/**
+ * Defines the "readtime" plugin.
+ *
+ * @CKEditorPlugin(
+ *   id = "readtime",
+ *   label = @Translation("Read time calculator")
+ * )
+ */
+class ReadTime extends Wordcount {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFile() {
+    return drupal_get_path('module', 'suopa_editorial') . "/assets/js/plugin.js";
+  }
+}
+
