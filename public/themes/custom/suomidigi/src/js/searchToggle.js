aa6689e3f0001f2fa1e8478ecb448ce17017b056
Sebastian <sebastian@hamse.fi>
2019-07-08 14:24:50 +0300

    Update search input desktop styling

--- public/themes/custom/suomidigi/src/js/searchToggle.js
+++ public/themes/custom/suomidigi/src/js/searchToggle.js
@@ -2,9 +2,23 @@
   Drupal.behaviors.searchToggle = {
     attach: function(context, settings) {
       const searchToggleButton = document.querySelector("#search-trigger");
+      const searchFormParentElement = document.querySelector(".search-form-container");
+
+      if (window.innerWidth > 769) {
+        searchFormParentElement.classList.remove("is-hidden");
+      } else {
+        searchFormParentElement.classList.add("is-hidden");
+      }
+
+      window.addEventListener("resize", () => {
+        if (window.innerWidth > 769) {
+          searchFormParentElement.classList.remove("is-hidden");
+        } else {
+          searchFormParentElement.classList.add("is-hidden");
+        }
+      });
 
       searchToggleButton.addEventListener("click", () => {
-        const searchFormParentElement = document.querySelector(".search-form-container");
         const searchFormInput = document.getElementById("search");
         searchFormParentElement.classList.toggle("is-hidden");
         searchFormInput.focus();
