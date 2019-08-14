99db76da20c6016967fee6233595324b785d2e71
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2019-08-14 13:07:25 +0300

    Added logic to change the Expand all/Collapse all -button function when all cards have been manually expanded.

--- public/modules/custom/suopa_legislations/assets/js/expanding-panel.js
+++ public/modules/custom/suopa_legislations/assets/js/expanding-panel.js
@@ -1,11 +1,33 @@
 (($, Drupal) => {
   var headers = $('.legislation-collection__item .legislation-card__header');
   var expandLink = $('.legislation-collection__toggle-button');
+  var current, siblings, siblingsOpen;
 
   // React on header click.
   headers.click(function() {
     $(this).toggleClass('is-active');
     $(this).parents('.legislation-card').toggleClass('is-open');
+
+    // Get the amount of sibling elements.
+    current = $(this).parents('.legislation-card').hasClass('is-open');
+    siblings = $(this).parents('.legislation-card').siblings('.legislation-card').length;
+    siblingsOpen = $(this).parents('.legislation-card').siblings('.legislation-card.is-open').length;
+
+    // Change the text for toggle button based on the amount of open cards.
+    if (current && siblings === siblingsOpen) {
+      $(this)
+        .parents('.legislation-collection__item')
+        .children('.legislation-collection__toggle-button')
+        .text(Drupal.t('Collapse all'))
+        .addClass('is-all-open');
+    } else {
+      $(this)
+        .parents('.legislation-collection__item')
+        .children('.legislation-collection__toggle-button')
+        .text(Drupal.t('Expand all'))
+        .removeClass('is-all-open');
+    }
+
     return false;
   });
 
@@ -24,3 +46,4 @@
     }
   });
 })(jQuery, Drupal);
+
