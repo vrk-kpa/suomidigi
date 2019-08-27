0d509a33746f51ffb8c7d3d54616734ecbf62547
Arto Iijalainen <arto.iijalainen@gmail.com>
2019-08-27 14:50:28 +0300

    Base commit for Community custom entity.

---
+++ public/modules/custom/suopa_communities/src/Form/CommunityDeleteForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Drupal\suopa_communities\Form;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+
+/**
+ * Provides a form for deleting Community entities.
+ *
+ * @ingroup suopa_communities
+ */
+class CommunityDeleteForm extends ContentEntityDeleteForm {
+
+
+}
