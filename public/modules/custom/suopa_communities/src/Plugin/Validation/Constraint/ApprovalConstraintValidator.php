1f1724158d3221bd154dd8ea0371e1bc9297849f
Arto Iijalainen <arto.iijalainen@gmail.com>
2019-09-03 15:00:00 +0300

    Added an approval constraint to the Community entity type.

---
+++ public/modules/custom/suopa_communities/src/Plugin/Validation/Constraint/ApprovalConstraintValidator.php
@@ -0,0 +1,29 @@
+<?php
+namespace Drupal\suopa_communities\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ApprovalConstraintValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($entity, Constraint $constraint) {
+    if (!isset($entity)) {
+      return;
+    }
+
+    // The entity has to have the status (published) field and the
+    // approved field.
+    if (!$entity->hasField('status') || !$entity->hasField('field_approved')) {
+      return;
+    }
+
+    // Entity has to be approved in order to be published.
+    if ($entity->get('status')->value == TRUE &&
+      $entity->get('field_approved')->value == FALSE) {
+      $this->context->addViolation($constraint->message);
+    }
+  }
+}
