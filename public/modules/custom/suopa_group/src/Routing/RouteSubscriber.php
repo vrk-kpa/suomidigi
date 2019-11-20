16e8a09c6dc49381df5e7cea49812c7671ba25db
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2019-11-20 16:45:29 +0200

    Added SuoPa group module and updated settings for group.

---
+++ public/modules/custom/suopa_group/src/Routing/RouteSubscriber.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\suopa_group\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubsciber.
+ *
+ * @package Drupal\suopa_group\Routing
+*/
+class RouteSubscriber extends RouteSubscriberBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function alterRoutes(RouteCollection $collection) {
+    if ($route = $collection->get('view.group_members.page_1')) {
+      $route->addOptions(['_admin_route' => TRUE]);
+    }
+    if ($route = $collection->get('view.group_nodes.page_1')) {
+      $route->addOptions(['_admin_route' => TRUE]);
+    }
+  }
+
+}
