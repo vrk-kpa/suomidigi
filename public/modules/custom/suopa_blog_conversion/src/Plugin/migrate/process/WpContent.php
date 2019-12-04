0ced7b3eaa26c6143663fabeb65355e2837e8c7c
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2019-12-04 14:41:40 +0200

    Added preg_replace for image urls.

--- public/modules/custom/suopa_blog_conversion/src/Plugin/migrate/process/WpContent.php
+++ public/modules/custom/suopa_blog_conversion/src/Plugin/migrate/process/WpContent.php
@@ -19,7 +19,11 @@ class WpContent extends ProcessPluginBase {
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    return _filter_autop($this->convertCaption($value));
+    return _filter_autop(
+      $this->convertCaption(
+        $this->convertImageUrls($value)
+      )
+    );
   }
 
   /**
@@ -34,9 +38,41 @@ class WpContent extends ProcessPluginBase {
   private function convertCaption($content) {
     return preg_replace(
       '/\[caption([^\]]+)align="([^"]+)"\s+width="(\d+)"\](\s*\<img[^>]+>)\s*(.*?)\s*\[\/caption\]/i',
-      '<div\1style="width: \3px" class="wp-caption \2">\4<p class="caption">\5</p></div>',
+      '<div\1 class="wp-caption \2">\4<p class="caption">\5</p></div>',
       $content
     );
+  }
+
+  /**
+   * Point images to local files instead of WP urls.
+   *
+   * @param string $content
+   *   Content as string.
+   *
+   * @return string
+   *   Returns same content without pesky captions.
+   */
+  private function convertImageUrls($content) {
+    return preg_replace_callback(
+      '/<img\s+.*?src=[\"\']?([^\"\' >]*)[\"\']?[^>]*>/i',
+      [&$this, 'convertUrl'],
+      $content
+    );
+  }
+
+  /**
+   * Static function for preg replace callback.
+   *
+   * @param array $src
+   *   URL pieces.
+   *
+   * @return array
+   *   Fixed URL pieces.
+   */
+  public static function convertUrl($src) {
+    $url = end(array_keys($src));
+    $src[$url] = '/sites/default/files/images/' . end(explode('/', $src[$url]));
+    return $src;
   }
 
 }
