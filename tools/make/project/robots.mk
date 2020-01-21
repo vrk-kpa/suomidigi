753f709a7ebbcd917413f9c8861e92400b0c7a18
Pasi Järnstedt <pasi.jarnstedt@gmail.com>
2020-01-21 13:04:50 +0200

    One robots.txt for all environments. Updated make scripts.

--- tools/make/project/robots.mk
+++ tools/make/project/robots.mk
@@ -2,7 +2,6 @@ BUILD_TARGETS += copy-robots
 
 PHONY += copy-robots
 copy-robots: ## Copy robots.txt
-ifeq ($(ENV),testing)
-	$(call colorecho, "\n-Copying robots.txt for testing env.\n")
-	cp ./conf/env_testing/robots.txt ./public/robots.txt
-endif
+    $(call colorecho, "\n-Copying robots.txt for all environments.\n")
+    cp ./conf/robots.txt ./public/robots.txt
+
