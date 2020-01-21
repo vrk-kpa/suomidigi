198108c34f87fd883e8071efae54b5d71104f1ae
Pasi Järnstedt <pasi.jarnstedt@gmail.com>
2020-01-21 13:09:53 +0200

    Indentation with tabs.

--- tools/make/project/robots.mk
+++ tools/make/project/robots.mk
@@ -2,6 +2,5 @@ BUILD_TARGETS += copy-robots
 
 PHONY += copy-robots
 copy-robots: ## Copy robots.txt
-    $(call colorecho, "\n-Copying robots.txt for all environments.\n")
-    cp ./conf/robots.txt ./public/robots.txt
-
+	$(call colorecho, "\n-Copying robots.txt for all environments.\n")
+	cp ./conf/robots.txt ./public/robots.txt
