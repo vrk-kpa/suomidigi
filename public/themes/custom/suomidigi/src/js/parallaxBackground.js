9fd41aa0b8282246daf900dbf46a7ba03bd1b62b
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2020-01-23 15:43:55 +0200

    Fixed mobile menu.

--- public/themes/custom/suomidigi/src/js/parallaxBackground.js
+++ public/themes/custom/suomidigi/src/js/parallaxBackground.js
@@ -3,12 +3,12 @@
 (function ($, Drupal) {
   Drupal.behaviors.parallaxBackground = {
     attach: function attach(context) {
-      $(window).once().on('load', function () {
+      $(window, context).on('load', function () {
         var background = document.getElementsByClassName('page-main')[0];
         window.addEventListener('scroll', function () {
           var scrollTop = window.pageYOffset || window.scrollTop;
           var scrollPercent = scrollTop / window.innerHeight * 0.15 || 0;
-          background.style.backgroundPosition = '50%' + '-' + scrollPercent * window.innerWidth + 'px';
+          background.style.backgroundPositionY = '-' + scrollPercent * window.innerWidth + 'px';
         });
       });
     }
