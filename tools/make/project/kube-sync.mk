8f2482fb22e9c2caa5ab47c9c4a2ca2a6625ff09
Pasi Järnstedt <pasi.jarnstedt@gmail.com>
2020-03-23 16:38:50 +0200

    Fixed comments and database loading.

--- tools/make/project/kube-sync.mk
+++ tools/make/project/kube-sync.mk
@@ -2,12 +2,12 @@
 PHONY += kube-sync
 kube-sync: ## Sync database and files
 	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...)
-	# Copy
+	## Copy
 	kubectl.dvv-suomidigi-beta exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql'
-	# Copy current database to local machine
+	## Copy current database to local machine
 	kubectl.dvv-suomidigi-beta cp suomidigi-drupal-0:/tmp/current.sql files_private/current.sql
-	# Copy current database to docker container
-	$(call drush_on_docker, sql-cli < files_private/current.sql)
+	## Import local database
+	$(call drush_on_docker, sql-cli < ../files_private/current.sql)
 #	joku_muu_tapa_synkata_db
 	$(call step,Sync files from @$(DRUPAL_SYNC_SOURCE)...)
 #	joku_muu_tapa_synkata_filut
