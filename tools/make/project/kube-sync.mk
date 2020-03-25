ab26efa3a4fbff49d84abc97b973dacd46843c57
Pasi Järnstedt <pasi.jarnstedt@gmail.com>
2020-03-25 12:01:33 +0200

    Transfer zipped database.

--- tools/make/project/kube-sync.mk
+++ tools/make/project/kube-sync.mk
@@ -4,10 +4,12 @@ DRUPAL_SYNC_SOURCE = AWS-production
 
 kube-sync: ## Sync database and files
 	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...)
-	## Copy
-	kubectl.dvv-suomidigi-prod exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql'
-	## Copy current database to local machine
-	kubectl.dvv-suomidigi-prod cp suomidigi-drupal-0:/tmp/current.sql files_private/current.sql
-	## Import local database
+## Copy
+	kubectl.dvv-suomidigi-prod exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql && gzip -f /tmp/current.sql'
+## Copy current database to local machine
+	kubectl.dvv-suomidigi-prod cp suomidigi-drupal-0:/tmp/current.sql.gz files_private/current.sql.gz
+	gunzip -f files_private/current.sql.gz
+## Import local database
+	$(call step, Importing database...)
 	$(call drush_on_docker, sql-cli < ../files_private/current.sql)
-	## Don't sync files, since stage file proxy
+## Don't sync files, since stage file proxy
