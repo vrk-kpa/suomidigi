3d3b043191e03bd2777aeccfb99c9eb78f2238ce
Pasi Järnstedt <pasi.jarnstedt@gmail.com>
2020-03-25 11:38:51 +0200

    Enable kube-sync in make fresh.

--- tools/make/project/kube-sync.mk
+++ tools/make/project/kube-sync.mk
@@ -1,13 +1,13 @@
-#SYNC_TARGETS := custom-sync
+SYNC_TARGETS := kube-sync
 PHONY += kube-sync
+DRUPAL_SYNC_SOURCE = AWS-production
+
 kube-sync: ## Sync database and files
 	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...)
 	## Copy
-	kubectl.dvv-suomidigi-beta exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql'
+	kubectl.dvv-suomidigi-prod exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql'
 	## Copy current database to local machine
-	kubectl.dvv-suomidigi-beta cp suomidigi-drupal-0:/tmp/current.sql files_private/current.sql
+	kubectl.dvv-suomidigi-prod cp suomidigi-drupal-0:/tmp/current.sql files_private/current.sql
 	## Import local database
 	$(call drush_on_docker, sql-cli < ../files_private/current.sql)
-#	joku_muu_tapa_synkata_db
-	$(call step,Sync files from @$(DRUPAL_SYNC_SOURCE)...)
-#	joku_muu_tapa_synkata_filut
+	## Don't sync files, since stage file proxy
