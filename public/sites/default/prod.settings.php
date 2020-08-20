bd4d2a25339281bb62733bdd0f5a0d4f1b2a7a08
Arto Iijalainen <arto.iijalainen@gmail.com>
2020-08-20 10:48:36 +0300

    Added Switfmailer overrides SMTP -> native PHP transport if SMTP config not present.

--- public/sites/default/prod.settings.php
+++ public/sites/default/prod.settings.php
@@ -8,3 +8,8 @@
 
 // Disabling stage file proxy on production, with that the module can be enabled even on production
 $config['stage_file_proxy.settings']['origin'] = false;
+
+// Use PHP transport for mails unless SMTP configuration is present.
+if (!isset($_ENV['SMTP_ADDRESS'])) {
+  $config['swiftmailer.transport']['transport'] = 'native';
+}
