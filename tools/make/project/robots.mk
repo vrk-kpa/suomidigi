1e468af03aae7cf6d2761892bd9b115202bfafd7
Marko Korhonen <marko.korhonen@druid.fi>
2022-03-16 12:23:41 +0200

    Use copy func

--- tools/make/project/robots.mk
+++ tools/make/project/robots.mk
@@ -2,5 +2,5 @@ BUILD_TARGETS += copy-robots
 
 PHONY += copy-robots
 copy-robots: ## Copy robots.txt
-	$(call step, Copying robots.txt for all environments.)
-	cp ./conf/robots.txt ./public/robots.txt
+	$(call step,Copying robots.txt for all environments.\n)
+	$(call copy,conf/robots.txt,public/robots.txt)
