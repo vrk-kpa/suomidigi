1414bb1f60e988c2405be261c702984ca52ae13b
Marko Korhonen <marko.korhonen@druid.fi>
2022-06-16 09:14:52 +0300

    Remove modules which are not installed

--- public/sites/default/dev.settings.php
+++ public/sites/default/dev.settings.php
@@ -1,10 +1,5 @@
 <?php
 
-// Disable Google Analytics from sending dev GA data.
-if (getenv('HTTP_HOST') === 'suomidigi.docker.so') {
-  $config['google_analytics.settings']['account'] = 'UA-XXXXXXXX-YY';
-}
-
 $settings['cache']['bins']['render'] = 'cache.backend.null';
 $settings['cache']['bins']['page'] = 'cache.backend.null';
 $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
