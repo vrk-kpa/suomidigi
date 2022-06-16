50965c02d88e148591a1a23d5ba77ce85e42fecb
Marko Korhonen <marko.korhonen@druid.fi>
2022-06-16 08:14:06 +0300

    For time being, allow php 7.3, 7.4, 8.0 and 8.1 in Composer

--- tools/make/project/kube-sync.mk
+++ tools/make/project/kube-sync.mk
@@ -4,12 +4,8 @@ DRUPAL_SYNC_SOURCE = AWS-production
 
 kube-sync: ## Sync database and files
 	$(call step,Sync database from @$(DRUPAL_SYNC_SOURCE)...\n)
-## Copy
-	kubectl.dvv-suomidigi-prod exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql && gzip -f /tmp/current.sql'
-## Copy current database to local machine
-	kubectl.dvv-suomidigi-prod cp suomidigi-drupal-0:/tmp/current.sql.gz files_private/current.sql.gz
-	gunzip -f files_private/current.sql.gz
-## Import local database
+	@kubectl.dvv-suomidigi-prod exec suomidigi-drupal-0 -- bash -c 'mysqldump -h $$DRUPAL_DB_HOST -p$$DRUPAL_DB_PASS -u $$DRUPAL_DB_USER $$DRUPAL_DB_NAME > /tmp/current.sql && gzip -f /tmp/current.sql'
+	@kubectl.dvv-suomidigi-prod cp suomidigi-drupal-0:/tmp/current.sql.gz files_private/current.sql.gz
+	@gunzip -f files_private/current.sql.gz
 	$(call step,Importing database...\n)
 	$(call drush,sql-cli < ../files_private/current.sql)
-## Don't sync files, since stage file proxy
