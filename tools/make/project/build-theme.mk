9f1ec6641c65edb67fe7017b59411fc68851b5d0
Marko Korhonen <marko.korhonen@druid.fi>
2023-02-14 12:36:42 +0200

    make drupal-build-theme|drupal-watch-theme with Donn

--- tools/make/project/build-theme.mk
+++ tools/make/project/build-theme.mk
@@ -1,11 +1,11 @@
-PHONY += build-theme
-build-theme: ## Build theme
-	$(call step,Build theme...\n)
-	$(call node_run,run gulp development)
+THEME_PATH := $(shell pwd)/$(WEBROOT)/themes/custom/suomidigi
 
-#PHONY += build-theme
-#build-theme: DOCKER_NODE_IMG ?= node:16-alpine
-#build-theme: WD := /usr/src/app
-#build-theme: ## Install NPM packages and build theme
-#	$(call step,Build theme...\n)
-#	@docker run --rm -v "$(CURDIR)":$(WD) -w $(WD) $(DOCKER_NODE_IMG) npm run --prefix ./public/themes/custom/suomidigi gulp development
+PHONY += drupal-build-theme
+drupal-build-theme:
+	$(call step,Build theme with Gulp...\n)
+	@docker run -it --rm -v $(THEME_PATH):/data druidfi/donn:node-$(NODE_VERSION) gulp production
+
+PHONY += drupal-watch-theme
+drupal-watch-theme:
+	$(call step,Watch theme with Gulp...\n)
+	@docker run -it --rm -v $(THEME_PATH):/data druidfi/donn:node-$(NODE_VERSION) gulp watch
